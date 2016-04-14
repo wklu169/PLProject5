744ad71f902da98b9e6f42a1c04e34866e5eabc7
Wilson Lu <wklu169@gmail.com>
2016-04-13 23:58:51 -0500

    Finished Problem 2

--- PLY/Project5.py
+++ PLY/Project5.py
@@ -1,11 +1,20 @@
-tokens = ('BOOK', 'STATS', 'NAME', 'NUMBERS')
-literals = ['.', ':' ]
+tokens = ('BOOK', 'NAME', 'STR', 'THEME', 'BY', 'DATE', 'FOR', 'WHERE', 'STATS', 'CHAPTERS', 'VERSES', 'WORDS', 'MISC', 'INTEGER')
+literals = ['.', ':', ',' ]
 
 # Tokens
-t_BOOK  = r'BOOK NAME.*'
-t_STATS =r'STATISTICS.*'
-t_NAME = r'[A-Z][a-z]+|[A-Z][a-z]+\s[A-Z][a-z]+'
-t_NUMBERS = r'[0-9]+\schapters,\s[0-9]+\sverses,\s[0-9]+\swords'
+t_BOOK = r'BOOK'
+t_NAME = r'NAME'
+t_STR = r'[A-Z][a-z]+.*'
+t_THEME = r'^THEME.*$'
+t_BY = r'^BY.*$'
+t_DATE = r'^DATE.*$'
+t_FOR = r'^FOR.*$'
+t_WHERE = r'^WHERE.*$'
+t_STATS = r'^STATISTICS'
+t_CHAPTERS = r'chapters*'
+t_VERSES = r'verses'
+t_WORDS = r'words'
+t_MISC = r'^MISC.*$'
 
 # Ignored characters
 t_ignore = " \r"
@@ -15,7 +24,7 @@ def t_newline(t):
     t.lexer.lineno += t.value.count("\n")
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    # print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 def t_INTEGER(t):
@@ -37,12 +46,18 @@ global time_step
 time_step = 0
 
 def p_start(t):
-    '''start : BOOK
-             | STATS
-             | NAME
-             | NUMBERS
+    '''start : booknum
+             | book
+             | THEME
+             | BY
+             | DATE
+             | FOR
+             | WHERE
+             | data
+             | MISC
+             | empty
     '''
-    print("Saw: ", t[1])
+    # print("Saw: ", t[1])
 
 def p_error(t):
     if t == None:
@@ -50,28 +65,33 @@ def p_error(t):
     else:
         print("Syntax error at '%s'" % t.value)
 
-def p_book(t):
-    'book : BOOK '
-    print('saw book')
+def p_booknum(t):
+    'booknum : BOOK INTEGER'
+    t[0]= str(t[1]) + ' ' + str(t[2])
+    print (t[0])
 
-def p_header(t):
-    'header : BOOK ":" NAME'
-    t[0] =  str(t[1]) + str(t[2])
-    print(str(t[0]))
+def p_book(t):
+    'book : BOOK NAME ":" STR'
+    t[0]= str(t[4])
+    print (t[0])
 
 def p_data(t):
-    'data : STATS ":" NUMBERS'
-    t[0] =  str(t[1]) + str(t[2])
-    print(str(t[0]))
+    'data : STATS ":" INTEGER CHAPTERS "," INTEGER VERSES "," INTEGER WORDS'
+    t[0] = str(t[3]) + ' ' + str(t[4]) + ' ' + str(t[6]) + ' ' + str(t[7]) + ' ' + str(t[9]) + ' ' + str(t[10])
+    print (t[0])
+
+def p_empty(t):
+    'empty : '
+    pass
 
 import ply.yacc as yacc   # ply.yacc comes from the ply folder in the PLY download.
 parser = yacc.yacc()
 
 while True:
     try:
-        s = raw_input('')
+        s = input('')
     except EOFError:
         break
     parser.parse(s)
 
-# To run the parser do the following in a terminal window: plyParserInputs/mpstat.out | python Project5.py
+# To run the parser do the following in a terminal window: cat biblestats.txt | python Project5.py
--- PLY/parsetab.py
+++ PLY/parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.8'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'ECD88615902865D89A5FBA42406F93CF'
+_lr_signature = 'F6684FFC6A3F9743CD609A6E3CDC27FB'
     
-_lr_action_items = {'ALL':([2,23,],[10,-6,]),'AMPM':([21,],[23,]),'.':([13,],[16,]),'LINUX':([0,],[4,]),'INTEGER':([0,2,9,12,14,16,17,18,19,20,22,23,24,25,26,27,],[5,9,13,15,13,19,13,21,-10,13,13,-6,13,13,13,13,]),':':([5,15,],[12,18,]),'CPU':([2,23,],[11,-6,]),'$end':([0,1,3,4,6,7,8,10,11,19,28,],[-11,-3,0,-1,-5,-2,-4,-8,-7,-10,-9,]),}
+_lr_action_items = {'$end':([0,1,2,3,4,5,7,9,10,11,12,13,15,19,26,],[-14,-10,-2,-1,-7,-3,-9,-4,0,-6,-5,-8,-11,-12,-13,]),'NAME':([6,],[14,]),'FOR':([0,],[11,]),'WORDS':([25,],[26,]),'WHERE':([0,],[4,]),'MISC':([0,],[7,]),'BOOK':([0,],[6,]),'THEME':([0,],[5,]),'CHAPTERS':([18,],[20,]),',':([20,23,],[21,24,]),'BY':([0,],[9,]),'VERSES':([22,],[23,]),'INTEGER':([6,16,21,24,],[15,18,22,25,]),'STR':([17,],[19,]),'DATE':([0,],[12,]),':':([8,14,],[16,17,]),'STATS':([0,],[8,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'all':([0,],[1,]),'float':([9,14,17,20,22,24,25,26,27,],[14,17,20,22,24,25,26,27,28,]),'start':([0,],[3,]),'time':([0,],[2,]),'data':([0,],[6,]),'cpu':([0,],[7,]),'empty':([0,],[8,]),}
+_lr_goto_items = {'booknum':([0,],[3,]),'empty':([0,],[1,]),'start':([0,],[10,]),'book':([0,],[2,]),'data':([0,],[13,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,15 +26,18 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> start","S'",1,None,None,None),
-  ('start -> LINUX','start',1,'p_start','PLYmpstat.py',49),
-  ('start -> cpu','start',1,'p_start','PLYmpstat.py',50),
-  ('start -> all','start',1,'p_start','PLYmpstat.py',51),
-  ('start -> empty','start',1,'p_start','PLYmpstat.py',52),
-  ('start -> data','start',1,'p_start','PLYmpstat.py',53),
-  ('time -> INTEGER : INTEGER : INTEGER AMPM','time',6,'p_time','PLYmpstat.py',58),
-  ('cpu -> time CPU','cpu',2,'p_cpu','PLYmpstat.py',62),
-  ('all -> time ALL','all',2,'p_all','PLYmpstat.py',66),
-  ('data -> time INTEGER float float float float float float float float float','data',11,'p_data','PLYmpstat.py',70),
-  ('float -> INTEGER . INTEGER','float',3,'p_float','PLYmpstat.py',74),
-  ('empty -> <empty>','empty',0,'p_empty','PLYmpstat.py',78),
+  ('start -> booknum','start',1,'p_start','Project5.py',49),
+  ('start -> book','start',1,'p_start','Project5.py',50),
+  ('start -> THEME','start',1,'p_start','Project5.py',51),
+  ('start -> BY','start',1,'p_start','Project5.py',52),
+  ('start -> DATE','start',1,'p_start','Project5.py',53),
+  ('start -> FOR','start',1,'p_start','Project5.py',54),
+  ('start -> WHERE','start',1,'p_start','Project5.py',55),
+  ('start -> data','start',1,'p_start','Project5.py',56),
+  ('start -> MISC','start',1,'p_start','Project5.py',57),
+  ('start -> empty','start',1,'p_start','Project5.py',58),
+  ('booknum -> BOOK INTEGER','booknum',2,'p_booknum','Project5.py',69),
+  ('book -> BOOK NAME : STR','book',4,'p_book','Project5.py',73),
+  ('data -> STATS : INTEGER CHAPTERS , INTEGER VERSES , INTEGER WORDS','data',10,'p_data','Project5.py',77),
+  ('empty -> <empty>','empty',0,'p_empty','Project5.py',81),
 ]
